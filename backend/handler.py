90ee8612603b6dccf9e9b1216c28d985c0ca6714
Niklas <niklas@lindorfer.com>
2021-07-03 09:22:43 +0100

    Casting parameters to int and error handling

--- backend/handler.py
+++ backend/handler.py
@@ -6,15 +6,21 @@ from datetime import datetime
 
 def lambda_handler(event, context):
 
-    params = event["queryStringParameters"]
-    start = datetime.fromtimestamp(params["start"])
-    end = datetime.fromtimestamp(params["end"])
+    try:
+        params = event["queryStringParameters"]
+        start = datetime.fromtimestamp(int(params["start"]))
+        end = datetime.fromtimestamp(int(params["end"]))
+    except:
+        return {
+            "statusCode": 400,
+            "body": "Missing or incorrect headers"
+        }
     
     dynamodb = resource("dynamodb")
     table = dynamodb.Table(os.environ["TABLE_NAME"])
     
     response = table.query(
-        KeyConditionExpression=(Key("timestamp").lte(end) & Key("timestamp").gte(start))
+        KeyConditionExpression=(Key("timestamp").lte(end.isoformat()) & Key("timestamp").gte(start.isoformat()))
     )
 
     return {
